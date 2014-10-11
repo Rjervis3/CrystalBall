3f3019c9391f96f53663b1d235d76e8fa638d5bc
Rjervis3 <jervisre@grinnell.edu>
2014-10-11 14:08:54 -0500

    Initial Commit

---
+++ CrystalBall/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CrystalBall
+//
+//  Created by Renn Jervis on 10/11/14.
+//  Copyright (c) 2014 Renn Jervis. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CrystalBall/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CrystalBall
+//
+//  Created by Renn Jervis on 10/11/14.
+//  Copyright (c) 2014 Renn Jervis. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
